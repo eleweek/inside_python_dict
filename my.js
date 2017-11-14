1698c02f175779b340a99aa4a78977b0ceaad022
Aleksandr Putilin <avp-13@yandex.ru>
2017-11-15 02:20:57 +0300

    Implement py hash function for strings

--- my.js
+++ my.js
@@ -1,3 +1,184 @@
+class Int64 {
+    constructor(jsNumInt32 = 0) {
+        this.size = 64;
+        this.jsNumMaxSize = 32;
+
+        this.data = [];
+        var signBit = jsNumInt32 >= 0 ? 0 : 1;
+
+        for (var i = 0; i < this.jsNumMaxSize; ++i) {
+            var bit = (jsNumInt32 & (1 << i)) ? 1 : 0;
+            this.data.push(bit);
+        }
+        
+        for (var i = this.jsNumMaxSize; i < this.size; ++i) {
+            this.data.push(signBit);
+        }
+    }
+    
+    xorBy(other) {
+        for (var i = 0; i < this.size; ++i) {
+            this.data[i] ^= other.data[i];
+        }
+
+        return this;
+    }
+
+    sign() {
+        return _.last(this.data) == 1 ? -1 : 1;
+    }
+
+    inc() {
+        this.data[0] += 1;
+        this._carryOverAll();
+    }
+
+    add(other) {
+        var carry = 0;
+        for (var i = 0; i < this.size; ++i) {
+            this.data[i] += other.data[i] + carry;
+            carry = (this.data[i] / 2) | 0;
+            this.data[i] %= 2;
+        }
+
+        return this;
+    }
+
+    complement() {
+        for (var i = 0; i < this.size; ++i) {
+            this.data[i] = (this.data[i] == 0 ? 1 : 0);
+        }
+
+        return this;
+    }
+
+    mulBy(other) {
+        // console.log("!");
+        var originalData = _.clone(this.data);
+        var otherData = _.clone(other.data);
+        // console.log("originalData =");
+        // console.log(originalData);
+        // console.log("otherData =");
+        // console.log(otherData);
+
+        for (var i = 0; i < this.size; ++i) {
+            this.data[i] = 0;
+        }
+
+        for (var i = 0; i < this.size; ++i) {
+            if (originalData[i] == 0) {
+                continue;
+            }
+
+            for (var j = 0; j < this.size; ++j) {
+                if (i + j < this.size) {
+                    this.data[i + j] += originalData[i] * otherData[j];
+                }
+            }
+
+            // console.log(this.data);
+        }
+
+        this._carryOverAll();
+        // console.log("CO =");
+        // console.log(this.data);
+
+        return this;
+    }
+
+    toNumber() {
+        var res = 0;
+        for (var i = 0; i < 32; ++i) {
+            if (this.data[i]) {
+                res |= (1 << i);
+            }
+        }
+
+        return res;
+    }
+
+    toString() {
+        var copyOfThis = _.cloneDeep(this);
+
+        var sign = copyOfThis.sign();
+        if (copyOfThis.sign() < 0) {
+            copyOfThis.complement().inc();
+        }
+
+        var decPower = [1];
+        var decRes = [0];
+        for (var i = 0; i < this.size; ++i) {
+            var carry = 0;
+            if (copyOfThis.data[i]) {
+                for (var j = 0; j < decPower.length; ++j) {
+                    if (j >= decRes.length)
+                        decRes.push(0);
+
+                    decRes[j] += decPower[j] + carry;
+                    carry = (decRes[j] / 10) | 0;
+                    decRes[j] %= 10;
+                }
+            }
+            if (carry) {
+                decRes.push(carry);
+            }
+
+            carry = 0;
+            for (var j = 0; j < decPower.length; ++j) {
+                decPower[j] = decPower[j] * 2 + carry;
+                carry = (decPower[j] / 10) | 0;
+                decPower[j] %= 10;
+            }
+            if (carry) {
+                decPower.push(carry);
+            }
+
+            /*console.log(decRes);
+            console.log(decPower);*/
+        }
+
+        var res = "";
+        if (sign < 0)
+            res += "-";
+        for (var j = decRes.length - 1; j >= 0; j--) {
+            res += String.fromCharCode("0".charCodeAt(0) + decRes[j]);
+        }
+
+        return res;
+    }
+
+    _carryOverAll() {
+        var carry = 0;
+        for (var i = 0; i < this.size; ++i) {
+            this.data[i] += carry;
+            carry = (this.data[i] / 2) | 0;
+            this.data[i] %= 2;
+        }
+    }
+}
+
+var py_hash_string = function(s) {
+    var res = new Int64(s.charCodeAt(0) << 7);
+    var magic = new Int64(1000003);
+
+    for (var i = 0; i < s.length; ++i) {
+        res = res.mulBy(magic).xorBy(new Int64(s.charCodeAt(i)));
+    }
+
+    res.xorBy(new Int64(s.length));
+
+    /* TODO XXX: compare to -1 */
+
+    return res.toString();
+}
+
+console.log(py_hash_string("a"))
+console.log(py_hash_string("aa"))
+console.log(py_hash_string("aaa"))
+console.log(py_hash_string("aaaa"))
+console.log(py_hash_string("abba"))
+console.log(py_hash_string("ilovepython"))
+
 Tangle.classes.TKArrayVis = {
     activeCellClass: 'array-cell-vis-active',
     cellClass: 'array-cell-vis',
@@ -41,6 +222,15 @@ Tangle.classes.TKArrayVis = {
 };
 
 $(document).ready(function() {
+    var x0 = 339;
+    var y0 = -922;
+    x = new Int64(x0);
+    y = new Int64(y0);
+    x.mulBy(y);
+    console.log("PLS");
+    console.log(x0 * y0);
+    console.log(x.toNumber());
+    console.log(x.toString());
     console.log("UWOTM8");
     var rootElement = document.getElementById('exampleArrayTangle');
     var model = {
