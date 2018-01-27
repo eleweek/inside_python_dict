493f6508e3802b9bc54d083967d03cd0dfc5ff36
Alexander Putilin <avp-13@yandex.ru>
2018-01-27 17:28:24 +0000

    Begin writing about python 3.2's version of dict and probing algorithm

--- index.js
+++ index.js
@@ -954,18 +954,25 @@ class App extends React.Component {
               <p> Let's see how we could resize the current table </p>
               TODO: visualization
               <p> There is still one more important question. Under what condition do we do a resizing? If we postpone resizing until table is nearly full, the performance severely degrades. If we do a resizing when the table is still sparse, we waste memory. </p>
-              <p> The search algorithm isn't changed much. We just get the hash value for the object, and then we also do the comparing hashes optimization during linear probing.  </p>
+              <p> And one more thing. The search algorithm isn't changed much. We just get the hash value for the object, and then we also do the comparing hashes optimization during linear probing.  </p>
               TODO: visualization
             
-              <h2> How does python dict work internally? </h2>
+              <h2> How does python dict *really* work internally? </h2>
               <p> Remember that this explanation is about dict in CPython (the most popular, "default", implementation of python), so there is no single dict implementation. But what about CPython? CPython is a single project, but there are multiple versions (2.7, 3.0, 3.2, 3.6, etc). The implementation of dict evolved over time, there were major improvements made data organization in 3.3 and 3.4, and the dict became "ordered" in 3.6. The string hash function was changed in 3.4. </p>
-              <p> The core idea is stayed the same. Python dict is internally is still a hash table. </p>
+              <p> However, the core idea is stayed the same. Python dict is internally is still a hash table. </p>
               <p> Let's start tackling major changes one by one. </p>
               
-              <h5> Dict in python 3.2</h5>
-              TODO
+              <h5> Probing algorithm</h5>
+              <p> The major difference in python dict of all versions is probing algorithm. The problem with linear probing is that it doesn't not mix up the values well for many patterns that can occur in the real data. For example patterns like 16, 0, 1, 2, 3, 4... cause many collisions. </p>
+              <p> It is very prone to clustering. There is a nice metaphor by Robert Lafore: it's like the crowd that gathers when someone faints at the shopping mall. The first arrivals come because they saw the victim fall; later arrivals gather because they wondered what everyone else was looking at. The larger the crowd grows, the more people are attracted to it. <a href="https://stackoverflow.com/questions/17386138/quadratic-probing-over-linear-probing"> From: stackoverflow. </a> </p>
+              TODO 
+              <p> If we use this probing algorithm instead of linear probing, we get python 3.2's version of dict. The only thing we need to add is handling of values, which is not that difficult. </p>
+              <p>
+              </p
+              <h5> Python 3.2's dict </h5>
+              <p> Let's see how this dict can be implemented. </p>
 
-              <h5> Dev stuff</h5>
+              <h5> Old dev stuff</h5>
               <p>
                 Let's say we want to add
                 <JsonInput inline={true} value={this.state.howToAddObj} onChange={(value) => this.setState({howToAddObj: value})} />
