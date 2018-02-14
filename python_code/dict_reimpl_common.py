bad5de62a56fa157100526f4e9e0c7a1fed2ff09
Alexander Putilin <avp-13@yandex.ru>
2018-02-14 00:30:48 +0000

    Refactor out common part of dict interface

---
+++ python_code/dict_reimpl_common.py
@@ -0,0 +1,51 @@
+from common import NULL, DUMMY
+
+
+class BaseDictImpl(object):
+    def __init__(self):
+        self.hashes = self._new_empty(self.START_SIZE)
+        self.keys = self._new_empty(self.START_SIZE)
+        self.values = self._new_empty(self.START_SIZE)
+        self.fill = 0
+        self.used = 0
+
+    @staticmethod
+    def _new_empty(size):
+        return [NULL for _ in range(size)]
+
+    def __delitem__(self, key):
+        idx = self.lookdict(key)
+
+        self.used -= 1
+        self.keys[idx] = DUMMY
+        self.values[idx] = NULL
+
+    def __getitem__(self, key):
+        idx = self.lookdict(key)
+
+        return self.values[idx]
+
+    def base_resize(self, quot=4):
+        old_hashes, old_keys, old_values = self.hashes, self.keys, self.values
+
+        new_size = 8
+        while new_size < quot * self.used:
+            new_size *= 2
+        self.hashes = self._new_empty(new_size)
+        self.keys = self._new_empty(new_size)
+        self.values = self._new_empty(new_size)
+
+        for h, k, v in zip(old_hashes, old_keys, old_values):
+            if h is not NULL and k is not NULL and k is not DUMMY:
+                self.insertdict_clean(k, v)
+
+        self.fill = self.used
+
+    def __setitem__(self, key, value):
+        fill_increased = self.insertdict_clean(key, value)
+        if fill_increased:
+            self.fill += 1
+        self.used += 1
+
+        if self.fill * 3 >= len(self.keys) * 2:
+            self.resize()
--- python_code/hash_chapter3_class_impl.py
+++ python_code/hash_chapter3_class_impl.py
@@ -2,46 +2,33 @@ from common import DUMMY, NULL
 from dict_reimpl_common import BaseDictImpl
 
 
-class PyDictReimplementation(BaseDictImpl):
+class HashDictImpl(BaseDictImpl):
     START_SIZE = 8
-    PERTURB_SHIFT = 5
 
     def __init__(self):
         BaseDictImpl.__init__(self)
 
-    @staticmethod
-    def _new_empty(size):
-        return [NULL for _ in range(size)]
-
-    @staticmethod
-    def signed_to_unsigned(hash_code):
-        return 2**64 + hash_code if hash_code < 0 else hash_code
-
     def lookdict(self, key):
         hash_code = hash(key)
-        perturb = self.signed_to_unsigned(hash_code)
 
         idx = hash_code % len(self.keys)
         while self.keys[idx] is not NULL:
             if self.hashes[idx] == hash_code and self.keys[idx] == key:
                 return idx
 
-            idx = (idx * 5 + perturb + 1) % len(self.keys)
-            perturb >>= self.PERTURB_SHIFT
+            idx = (idx + 1) % len(self.keys)
 
-        return NULL
+        raise KeyError()
 
     def insertdict_clean(self, key, value):
         hash_code = hash(key)
-        perturb = self.signed_to_unsigned(hash_code)
 
         idx = hash_code % len(self.keys)
         while self.keys[idx] is not NULL and self.keys[idx] is not DUMMY:
             if self.hashes[idx] == hash_code and self.keys[idx] == key:
                 break
 
-            idx = (idx * 5 + perturb + 1) % len(self.keys)
-            perturb >>= self.PERTURB_SHIFT
+            idx = (idx + 1) % len(self.keys)
 
         fill_increased = self.keys[idx] is NULL
 
@@ -52,9 +39,4 @@ class PyDictReimplementation(BaseDictImpl):
         return fill_increased
 
     def resize(self):
-        # TODO: proper target size (it is sometimes 2, sometimes 4 -- based on used)
-        return self.base_resize(4)
-
-
-def dump_py_reimpl_dict(d):
-    return d.hashes, d.keys, d.values
+        return self.base_resize(2)
