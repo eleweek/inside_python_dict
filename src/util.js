283daf29a33ad3a157f1452d062486e08bc61c16
Alexander Putilin <avp-13@yandex.ru>
2018-06-20 00:25:48 +0100

    Remove excessive logging on error boundaries

--- src/util.js
+++ src/util.js
@@ -3,8 +3,6 @@ import * as React from 'react';
 import {ErrorBoundary} from 'react-error-boundary';
 
 function MyFallbackComponent({componentStack, error}) {
-  console.log(error);
-    
   return <div style={{backgroundColor: "pink"}}>
     <h3 className="text-danger">An error occured. This should not happen.  Please file a bug report <a href="https://github.com/eleweek/inside_python_dict">on github</a></h3>
     <p>{componentStack}</p>
@@ -16,9 +14,6 @@ export function MyErrorBoundary(props) {
     const onError = (error, componentStack) => {
         console.log(componentStack);
         console.log(error);
-
-        console.log(typeof componentStack);
-        console.log(typeof error);
     }
 
     return <ErrorBoundary onError={onError} FallbackComponent={MyFallbackComponent}>
