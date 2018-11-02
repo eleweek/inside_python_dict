53e7bac64f20451d02d0912ea247564a9cc9ea47
Alexander Putilin <avp-13@yandex.ru>
2018-11-02 00:18:24 -0700

    Test for dict_factory from stress test (yep, tests for tests are necessary now :D)

---
+++ python_code/actual_dict_factory_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from dict32_reimplementation_test_v2 import dict_factory
+from dictinfo32 import dictobject, dump_py_dict
+
+
+def table_size(d):
+    return len(dump_py_dict(dictobject(d))[0])
+
+
+class TestDictFactory(unittest.TestCase):
+    def test_dict_factory(self):
+        self.assertEqual(table_size(dict_factory([])), 8)
+        self.assertEqual(table_size(dict_factory([(1, 1)])), 8)
+        self.assertEqual(table_size(dict_factory([(1, 1), (1, 2), (1, 3), (1, 4)])), 8)
+        self.assertEqual(table_size(dict_factory([(1, 1), (1, 2), (1, 3), (1, 4), (1, 5)])), 8)
+        self.assertEqual(table_size(dict_factory([(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8)])), 16)
+        self.assertEqual(table_size({1: 1, 1: 2, 1: 3, 1: 4, 1: 5, 1: 6, 1: 7, 1: 8}), 16)
+        self.assertEqual(table_size(dict([(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8)])), 8)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
--- unittest_python.sh
+++ unittest_python.sh
@@ -7,3 +7,4 @@ pyenv shell 3.2.6
 python3 python_code/hash_chapter2_impl_test.py
 python3 python_code/hash_chapter3_class_impl_test.py
 python3 python_code/interface_test.py
+python3 python_code/actual_dict_factory_test.py
