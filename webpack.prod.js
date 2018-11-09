ae919df7d1a871dd7c6ae2062eff9d450ef5a069
Alexander Putilin <avp-13@yandex.ru>
2018-11-08 23:59:18 -0800

    Remove uglify plugin in favour of default webpack stuff

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -10,9 +9,7 @@ module.exports = merge(common, {
     devtool: 'source-map',
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        new UglifyJSPlugin({
-            sourceMap: true,
-        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
