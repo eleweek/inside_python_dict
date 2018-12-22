e50f16120804505139f22d0fc3a1610014fc1c33
Alexander Putilin <avp-13@yandex.ru>
2018-12-22 19:36:48 +0000

    Get rid of generating templates in dev every time

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const {RawSource} = require('webpack-sources');
 const {exec} = require('child_process');
@@ -35,22 +34,6 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: 'build/chapter1.html',
-            filename: 'chapter1.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: 'build/chapter2.html',
-            filename: 'chapter2.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: 'build/chapter3.html',
-            filename: 'chapter3.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: 'build/chapter4.html',
-            filename: 'chapter4.html',
         }),
     ],
     watchOptions: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -10,6 +11,22 @@ module.exports = merge(common, {
         new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new HtmlWebpackPlugin({
+            template: 'build/chapter1.html',
+            filename: 'chapter1.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: 'build/chapter2.html',
+            filename: 'chapter2.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: 'build/chapter3.html',
+            filename: 'chapter3.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: 'build/chapter4.html',
+            filename: 'chapter4.html',
         }),
         new BundleAnalyzerPlugin(),
     ],
